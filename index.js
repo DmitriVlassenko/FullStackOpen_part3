39d75b8942e9f3bba2207fd8902e5910f7670005
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-03-15 22:16:43 +0200

    Phonebook backend step8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
+morgan.token('body', (req) => {
+    if (!req.body.name) {
+        return (
+            req.body = ':no data'
+        )}
+        else
+            return (JSON.stringify(req.body))
+})
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => {
     return (

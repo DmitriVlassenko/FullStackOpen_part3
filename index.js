704341c0f587d44ad68880008695ecd90704bf9b
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-03-15 18:22:12 +0200

    Phonebook backend step6

--- index.js
+++ index.js
@@ -68,9 +68,19 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (!body.name || !body.number) {
+    if (!body.name) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'Name is missing'
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'Number is missing'
+        })
+    }
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 

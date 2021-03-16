b6e1a45fc1c9cfcab7d9184bc2bf92eb114f8626
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-03-16 17:01:09 +0200

    uibuild

--- index.js
+++ index.js
@@ -44,10 +44,6 @@ let persons =
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Part3</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })

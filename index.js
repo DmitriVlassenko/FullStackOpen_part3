a26de4caf2b9f1c3c932a3b044f1be8de90f17c4
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-01 12:04:40 +0300

    uibuild

--- index.js
+++ index.js
@@ -53,31 +53,25 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'Name is missing'
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'Number is missing'
-        })
-    }
 
     const person = new Person ({
         name: body.name,
         number: body.number,
     })
-    person.save().then(savedPerson => response.json(savedPerson))
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -99,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
